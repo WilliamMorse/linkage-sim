5833d8478083c8297dcf4a5976c60187676f8915
will <william.b.morse@maine.edu>
2019-03-22 17:38:00 +0000

    Added back the polar to cartiseian functions.

--- vectors.jl
+++ vectors.jl
@@ -1,1 +1,16 @@
 println("hello there")
+
+function link_position(length, angle)
+    x = length*cos(angle)
+    y = lenght*sin(angle)
+    return (x,y)
+end
+
+function add_links(lengths, angles)
+    x = 0
+    y = 0
+    for (i, length) in enumerate(lengths)
+        x, y += link_position(length, angles[i])
+    end
+    return (x,y)
+end
