ea0c685807892ac39f985a1a0850acc8c4629507
will <william.b.morse@maine.edu>
2019-03-26 01:07:33 +0000

    testing link all function

--- vectors.jl
+++ vectors.jl
@@ -1,4 +1,12 @@
 println("hello there")
+using Plots
+
+function len(tuple)
+	for (i, element) in enumertate(tuple)
+		q = i
+	end
+	return q
+end
 
 function link_end_position(length, angle)
     x = length*cos(angle)
@@ -56,8 +64,53 @@ function circle_intersection(link1, link2)
 		end
 	end
 end
+
+function angle(x1,y1,x2,y2)
+	x = x2-x1
+	y = y2-y1
+	angle = atan2(x,y)
+	return angle
+end
+
 """
 println(circle_intersection((0,0,4),(5,0,1)))
 println(circle_intersection((0,0,4),(3,0,1)))
 println(circle_intersection((0,0,4),(3,0,2)))
+
+points then links
+then pins
+then motors
+then propogate
+
+why not start with the propogation?
+here's a simple point pattern:
 """
+
+function points()
+	a = ((0,0),(0,1),(2,0))
+	return a
+end
+
+
+function link_all(points)
+
+	lengths = ()
+	angles = ()
+	anchors = ()
+
+	numberOfPoints = len(points[:1])
+	for (i, point) in enumerate(points)
+		for j in i:numberOfPoints
+			x1,y1 = (points[i][1], points[i][2])
+			x2,y2 = (points[j][1], points[j][2])
+			lengths += sqrt(distance_between_points_2(x1,y1,x2,y2))
+			angles += angle(x1,y1,x2,y2)
+			anchors += (x1,y1)
+		end
+	end
+	return (anchors, lenghts, angles)
+end
+
+a = points()
+print(link_all(a))
+plot(rot(a))
